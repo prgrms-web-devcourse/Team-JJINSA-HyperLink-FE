b55eee00ec99a1a016145238ee65c668d43a1ac4
guno517 <guno5170@gmail.com>
2023-02-18 17:18:26 +0900

    chore: vanilla-extract plugin 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
