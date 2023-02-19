05d1f1469cbc0dd7de33051e83444595e72805c7
류건열 <rjsduf0503@naver.com>
2023-02-19 23:22:49 +0900

    feat: 아이콘 공통 컴포넌트 구현

--- src/components/common/icon/index.tsx
+++ src/components/common/icon/index.tsx
@@ -1,5 +1,24 @@
-const Icon = () => {
-  return <div></div>;
+import { CSSProperties } from 'react';
+
+type IconProps = {
+  type: string;
+  name: string;
+  size: number;
+  style?: CSSProperties;
+};
+
+const Icon = ({
+  type = 'solid',
+  name = 'xmark',
+  size,
+  ...props
+}: IconProps) => {
+  let classname = `fa-${type} fa-${name} ${name}`;
+  if (size) {
+    classname += ` fa-${size}x`;
+  }
+
+  return <i className={classname} style={{ ...props.style }} {...props}></i>;
 };
 
 export default Icon;
