c76e61b43213835cd6e3d25662856b53eb81d71d
류건열 <rjsduf0503@naver.com>
2023-02-21 14:58:56 +0900

    feat: 텍스트 컴포넌트 구현 및 storybook 연동

---
+++ src/stories/components/common/Text.stories.tsx
@@ -0,0 +1,39 @@
+import { Text } from '@/components/common';
+import { TextProps } from '@/components/common/text';
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  argTypes: {
+    block: {
+      defaultValue: false,
+      type: 'boolean',
+    },
+    paragraph: {
+      defaultValue: false,
+      type: 'boolean',
+    },
+    size: {
+      defaultValue: 'medium',
+      control: 'inline-radio',
+      options: ['xSmall', 'small', 'medium', 'large', 'xLarge'],
+      description: 'text size',
+    },
+    weight: {
+      defaultValue: 400,
+      control: 'inline-radio',
+      options: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      description: 'text weight',
+    },
+    color: {
+      defaultValue: '#9a9a9a',
+      control: 'color',
+      type: 'string',
+      description: 'text color',
+    },
+  },
+};
+
+export const Default = (args: TextProps) => {
+  return <Text {...args}>Text test</Text>;
+};
