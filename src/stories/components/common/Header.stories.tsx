2e2e9731efc40840a1ce8928532e46129ecc6792
류건열 <rjsduf0503@naver.com>
2023-02-21 16:48:01 +0900

    feat: 헤딩 컴포넌트 storybook 연동

---
+++ src/stories/components/common/Header.stories.tsx
@@ -0,0 +1,26 @@
+import { Heading } from '@/components/common';
+import { HeadingProps } from '@/components/common/heading';
+import * as variants from '@/styles/variants.css';
+
+export default {
+  title: 'Components/Heading',
+  component: Heading,
+  argTypes: {
+    level: {
+      defaultValue: 1,
+      control: 'inline-radio',
+      options: [1, 2, 3, 4, 5, 6],
+      description: 'heading level',
+    },
+    color: {
+      defaultValue: variants.vars.color.font.primary,
+      control: 'color',
+      type: 'string',
+      description: 'heading color',
+    },
+  },
+};
+
+export const Default = (args: HeadingProps) => {
+  return <Heading {...args}>Heading test</Heading>;
+};
