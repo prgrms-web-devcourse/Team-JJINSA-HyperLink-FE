6a4f37ce4a77ae1c48f62c7a37d2b3dc5f097d81
guno517 <guno5170@gmail.com>
2023-02-24 16:30:24 +0900

    refactor: chilren과 badge 구분

--- src/components/common/badge/index.tsx
+++ src/components/common/badge/index.tsx
@@ -1,29 +1,24 @@
+import { CSSProperties, ReactNode } from 'react';
 import * as style from './style.css';
 
 export type BadgeProps = {
+  children: ReactNode;
   hasText: boolean;
+  style?: CSSProperties;
 };
 
 // header AlramIcon badge, 알람 모달 badge / 데일리 브리핑 new badge
-const Badge = ({ hasText }: BadgeProps) => {
+const Badge = ({ hasText, children, ...props }: BadgeProps) => {
+  let badge = null;
+  if (hasText) {
+    badge = <sup className={style.Super({ type: 'text' })}>new</sup>;
+  } else {
+    badge = <sup className={style.Super({ type: 'dot' })} />;
+  }
   return (
-    <div className={style.badgeContainer}>
-      <div
-        className={style.badge(
-          hasText
-            ? {
-                size: 'text',
-                borderRadius: 'text',
-                display: 'flex',
-              }
-            : {
-                size: 'circle',
-                borderRadius: 'circle',
-              }
-        )}
-      >
-        {hasText && 'new'}
-      </div>
+    <div className={style.badgeContainer} style={{ ...props.style }}>
+      {children}
+      {badge}
     </div>
   );
 };
