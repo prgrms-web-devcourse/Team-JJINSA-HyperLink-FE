d2fb4007007bb083a44793bffa504d9abf9cb226
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-02-24 18:02:21 +0900

    chore: recoil, react-query, react-query-devtools 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,28 @@
 import '@/styles/global.css';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { RecoilRoot } from 'recoil';
 const { VITE_GOOGLE_CLIENT_ID } = import.meta.env;
 
 import App from './App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={VITE_GOOGLE_CLIENT_ID}>
       <BrowserRouter>
-        <App />
+        <RecoilRoot>
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <App />
+          </QueryClientProvider>
+        </RecoilRoot>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
