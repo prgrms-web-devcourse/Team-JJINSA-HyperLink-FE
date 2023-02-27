c29c95aa6180a58b9084425902b802ac9ec71d41
류건열 <rjsduf0503@naver.com>
2023-02-27 18:24:53 +0900

    remove: 중복 import 제거

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 const { VITE_GOOGLE_CLIENT_ID } = import.meta.env;
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 
