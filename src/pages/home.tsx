c45f0f7be50211576cbdbd4ed88364f4ee93a963
류건열 <rjsduf0503@naver.com>
2023-02-27 18:18:29 +0900

    remove: home 로직 삭제

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,5 @@
-import { LoginModal } from '@/components/modal';
-import { useState } from 'react';
-
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-  return (
-    <div>
-      Home 입니다~ <button onClick={() => setIsOpen(true)}>로그인 모달</button>
-      <LoginModal isOpen={isOpen} onClose={handleClose} />
-    </div>
-  );
+  return <div>Home 입니다~</div>;
 };
 
 export default Home;
