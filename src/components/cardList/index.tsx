fb14d4371dd247dca108d533c6d30a660692cb36
guno517 <guno5170@gmail.com>
2023-03-01 03:56:40 +0900

    feat: 홈 화면 msw 연동

--- src/components/cardList/index.tsx
+++ src/components/cardList/index.tsx
@@ -1,18 +1,10 @@
 import ArticleCard, { ArticleCardProps } from '@/components/cardItem/article';
-import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 import * as style from './style.css';
 
-type CardListProps = {
-  selectedCategory: string;
-  cards: ArticleCardProps[];
-};
-
-const CardList = ({ selectedCategory, cards }: CardListProps) => {
-  const { containerRef, cardList } = useInfiniteScroll(selectedCategory, cards);
-
+const CardList = ({ cards }: { cards: ArticleCardProps[] }) => {
   return (
-    <div ref={containerRef} className={style.listContainer}>
-      {cardList.map((card) => {
+    <div className={style.listContainer}>
+      {cards.map((card) => {
         return <ArticleCard key={card.contentId} {...card} />;
       })}
     </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,25 @@
+import CardList from '@/components/cardList';
+import { useEffect, useState } from 'react';
+
 const Home = () => {
-  return <div>Home 입니다~</div>;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch('/cardlist', {
+      headers: {
+        Accept: 'application / json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data);
+      });
+  }, []);
+  return (
+    <div style={{ margin: ' 3rem 10rem', minWidth: '60.6rem' }}>
+      <CardList cards={cards} />
+    </div>
+  );
 };
 
 export default Home;
