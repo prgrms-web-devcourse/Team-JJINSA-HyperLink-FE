8fce6f38497ce973cb1487ffbcbc3c0488269272
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-01 02:56:48 +0900

    feat: ArticleCard 컴포넌트로 검색 결과 랜더링

--- src/pages/searchResult/index.tsx
+++ src/pages/searchResult/index.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { getSearchContents } from '@/api/contents';
 import { useQuery } from '@tanstack/react-query';
 import { contents } from '@/types/contents';
+import ArticleCard from '@/components/cardItem/article';
 
 const searchResultPage = () => {
   const { keyword } = useParams() as { keyword: string };
@@ -21,14 +22,42 @@ const searchResultPage = () => {
   if (isError) return <h3>에러 발생</h3>;
   if (isLoading) return <h3>로딩 중...</h3>;
 
+  const { contents } = searchResult;
+
   return (
     <div>
       <h1>검색 결과 페이지 {keyword}</h1>
-      {searchResult.contents.length ? (
+      {contents.length ? (
         <ul>
-          {searchResult.contents.map(({ contentId, title }) => (
-            <li key={contentId}>{title}</li>
-          ))}
+          {contents.map(
+            ({
+              contentId,
+              contentImgUrl,
+              createdAt,
+              creator,
+              isBookmarked,
+              isLiked,
+              likeCount,
+              link,
+              title,
+              viewCount,
+              recommendationCompanies,
+            }) => (
+              <ArticleCard
+                key={contentId}
+                contentImgUrl={contentImgUrl}
+                isBookmarked={isBookmarked}
+                isLiked={isLiked}
+                likeCount={likeCount}
+                viewCount={viewCount}
+                link={link}
+                creator={creator}
+                createdAt={createdAt}
+                title={title}
+                recommendationCompanies={recommendationCompanies}
+              />
+            )
+          )}
         </ul>
       ) : (
         <p>검색 결과가 없습니다.</p>
