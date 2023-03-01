4f772a44384d2cf81afe4c59982b8f30c8ffe4db
EUNA <beni1026@naver.com>
2023-03-01 22:50:44 +0900

    feat: myInfo 컴포넌트 생성

---
+++ src/components/myPage/MyInfo.tsx
@@ -0,0 +1,69 @@
+import { Avatar, Button, Dropdown, Input } from '@/components/common';
+import * as style from './style.css';
+import { useState } from 'react';
+import useInput from '@/hooks/useInput';
+import { myInfo } from '@/types/myInfo';
+
+const careerItems = ['개발', '기획', '디자인'];
+const careerYearItems = ['1년 미만', '1년', '2년'];
+
+const MyInfo = ({ myInfo }: { myInfo: myInfo }) => {
+  const { email, nickname, profileImage, career, careerYear } = myInfo;
+
+  const [newProfileImage, setNewProfileImage] = useState(profileImage);
+  const { value: newNickname, onChange: onChangeNewNickname } =
+    useInput(nickname);
+  const [newCareer, setNewCareer] = useState(career);
+  const [newCareerYear, setNewCareerYear] = useState(careerYear);
+
+  const handleItemClick = (item: string, type: string) => {
+    switch (type) {
+      case 'career':
+        setNewCareer(item);
+        return;
+      case 'careerYear':
+        setNewCareerYear(item);
+        return;
+    }
+  };
+
+  const submitHandler = () => {
+    // 회원 정보 변경시 로직
+  };
+
+  return (
+    <>
+      <div className={style.avatarWrapper}>
+        <Avatar src={newProfileImage} shape="circle" size="xLarge" />
+      </div>
+      <Input type="email" label="이메일" value={email} readOnly />
+      <Input
+        label="닉네임"
+        value={newNickname}
+        onChange={onChangeNewNickname}
+      />
+      <div className={style.dropdownWrapper}>
+        <Dropdown
+          placeholder="선택해주세요"
+          label="직군/경력"
+          value={newCareer}
+          items={careerItems}
+          onItemClick={(item: string) => {
+            handleItemClick(item, 'career');
+          }}
+        />
+        <Dropdown
+          placeholder="선택해주세요"
+          value={newCareerYear}
+          items={careerYearItems}
+          onItemClick={(item: string) => {
+            handleItemClick(item, 'careerYear');
+          }}
+        />
+      </div>
+      <Button text="프로필 변경 완료" />
+    </>
+  );
+};
+
+export default MyInfo;
