29205ff224176ea9c6d59030293a3d68313bfd65
EUNA <beni1026@naver.com>
2023-03-01 22:51:43 +0900

    feat: App에 myPage 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-
 import Layout from '@/components/layout/index';
 import {
   HomePage,
   NotFoundPage,
   SearchResultPage,
   SignupPage,
+  MyPage,
 } from '@/pages/index';
 
 const router = createBrowserRouter(
@@ -20,6 +20,7 @@ const router = createBrowserRouter(
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/search/:keyword" element={<SearchResultPage />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Route>
   )
 );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as HomePage } from '@/pages/home';
 export { default as SignupPage } from '@/pages/sigunup';
 export { default as SearchResultPage } from '@/pages/searchResult';
 export { default as NotFoundPage } from '@/pages/notFound';
+export { default as MyPage } from '@/pages/myPage';
