8c24c65187b5a6c7cbad0cbb97fd2fa5f4803eae
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-04 01:10:14 +0900

    design: 특정 크리에이터 상세 페이지 UI 구현

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as HomePage } from '@/pages/home';
 export { default as SignupPage } from '@/pages/sigunup';
 export { default as SearchResultPage } from '@/pages/searchResult';
-export { default as NotFoundPage } from '@/pages/notFound';
+export { default as CreatorPage } from '@/pages/creator';
 export { default as MyPage } from '@/pages/myPage';
+export { default as NotFoundPage } from '@/pages/notFound';
