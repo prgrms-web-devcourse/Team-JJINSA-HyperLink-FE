78f5608cd10cd9be325346b44c79bdcc64fe2969
류건열 <rjsduf0503@naver.com>
2023-03-04 22:15:06 +0900

    rename: 폴더명 오타 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as HomePage } from '@/pages/home';
-export { default as SignupPage } from '@/pages/sigunup';
+export { default as SignupPage } from '@/pages/signup';
 export { default as SearchResultPage } from '@/pages/searchResult';
 export { default as NotFoundPage } from '@/pages/notFound';
 export { default as MyPage } from '@/pages/myPage';
