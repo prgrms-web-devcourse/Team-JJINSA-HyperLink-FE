53aa01eadcc51e4510cf2de6b33924c7180fce7d
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-05 18:40:07 +0900

    feat: Card 공통 컴포넌트 color prop 추가

--- src/components/common/card/index.tsx
+++ src/components/common/card/index.tsx
@@ -1,14 +1,18 @@
-import * as style from './style.css';
-
 import { ReactNode } from 'react';
+import * as style from './style.css';
 
 export type CardProps = {
   children: ReactNode;
-  type: 'creator' | 'content';
+  type: 'default' | 'creator' | 'content';
+  color?: string;
 };
 
-const Card = ({ children, type }: CardProps) => {
-  return <div className={style.CardWrapper({ type })}>{children}</div>;
+const Card = ({ children, type, color = '#fff' }: CardProps) => {
+  return (
+    <div className={style.CardWrapper({ type })} style={{ background: color }}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
