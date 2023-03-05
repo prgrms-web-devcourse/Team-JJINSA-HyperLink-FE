871ca6723c5a995556a5d1d516b7af84b6426b6e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-05 21:41:41 +0900

    design: 관심 카테고리별 회원 수, 콘텐츠 수 차트 구현

---
+++ src/components/dailyBriefing/ContentsCountChart.tsx
@@ -0,0 +1,78 @@
+import { Card, Heading } from '../common';
+import * as style from './style.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+  },
+};
+
+const labels = [
+  '23/03/06',
+  '23/03/07',
+  '23/03/08',
+  '23/03/09',
+  '23/03/10',
+  '23/03/11',
+  '23/03/12',
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: '누적 콘텐츠 수',
+      data: [10, 12, 34, 54, 56, 75, 84],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      tension: 0.5,
+    },
+  ],
+};
+
+const ContentsCountChart = () => {
+  return (
+    <Card
+      type="default"
+      color="#F7F9FB"
+      style={{ width: '100%', minWidth: '50rem' }}
+    >
+      <div className={style.title}>
+        <Heading level={6}>콘텐츠 수</Heading>
+      </div>
+      <div className={style.chart}>
+        <Line options={options} data={data} />
+      </div>
+    </Card>
+  );
+};
+
+export default ContentsCountChart;
