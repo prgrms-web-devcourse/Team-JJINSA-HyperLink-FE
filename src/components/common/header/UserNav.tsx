0ddbc05f96e3b492a6c0f434e79d894a6403f0d9
EUNA <beni1026@naver.com>
2023-03-06 14:45:18 +0900

    feat: 알림 아이콘 클릭 시 모달 뜨도록 구현

--- src/components/common/header/UserNav.tsx
+++ src/components/common/header/UserNav.tsx
@@ -4,12 +4,14 @@ import { MyInfoModal } from '@/components/modal';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { isAuthorizedState } from '@/stores/auth';
 import {
+  isAlarmModalVisibleState,
   isLoginModalVisibleState,
   isMyInfoModalVisibleState,
 } from '@/stores/modal';
 import { logout } from '@/api/auth';
 import * as style from './style.css';
 import user from '@/assets/user.svg';
+import AlarmModal from '@/components/modal/alarm';
 
 const UserNav = () => {
   const navigate = useNavigate();
@@ -17,12 +19,16 @@ const UserNav = () => {
   const [isMyInfoModalVisible, setIsMyInfoModalVisible] = useRecoilState(
     isMyInfoModalVisibleState
   );
+  const [isAlarmModalVisible, setIsAlarmModalVisible] = useRecoilState(
+    isAlarmModalVisibleState
+  );
   const setIsLoginModalVisible = useSetRecoilState(isLoginModalVisibleState);
 
   const handleLogout = async () => {
     await logout();
     setIsAuthorized(false);
     setIsMyInfoModalVisible(false);
+    setIsAlarmModalVisible(false);
 
     navigate('/');
   };
@@ -36,23 +42,35 @@ const UserNav = () => {
           onClick={() => setIsLoginModalVisible(true)}
         />
       ) : (
-        <div className={style.iconGroup}>
-          <Icon type="regular" name="pen-to-square" size="xLarge" />
-          <Icon type="regular" name="bell" size="xLarge" />
-          <button className={style.userIconButton} type="button">
-            <img
-              className={style.userIcon}
-              src={user}
-              alt="user image"
-              onClick={() => setIsMyInfoModalVisible((isVisible) => !isVisible)}
-            />
-            <MyInfoModal
-              isOpen={isMyInfoModalVisible}
-              onClose={() => setIsMyInfoModalVisible(false)}
-              onLogout={handleLogout}
-            />
-          </button>
-        </div>
+        <>
+          <div className={style.iconGroup}>
+            <Icon type="regular" name="pen-to-square" size="xLarge" />
+            <div
+              onClick={() => setIsAlarmModalVisible((isVisible) => !isVisible)}
+            >
+              <Icon type="regular" name="bell" size="xLarge" />
+            </div>
+            <button className={style.userIconButton} type="button">
+              <img
+                className={style.userIcon}
+                src={user}
+                alt="user image"
+                onClick={() =>
+                  setIsMyInfoModalVisible((isVisible) => !isVisible)
+                }
+              />
+              <MyInfoModal
+                isOpen={isMyInfoModalVisible}
+                onClose={() => setIsMyInfoModalVisible(false)}
+                onLogout={handleLogout}
+              />
+            </button>
+          </div>
+          <AlarmModal
+            isOpen={isAlarmModalVisible}
+            onClose={() => setIsAlarmModalVisible(false)}
+          />
+        </>
       )}
     </div>
   );
--- src/components/modal/alarm/style.css.ts
+++ src/components/modal/alarm/style.css.ts
@@ -4,15 +4,22 @@ import { style } from '@vanilla-extract/css';
 
 export const modalContent = style([
   utils.flexColumn,
+  utils.borderRadius,
+  utils.positionAbsolute,
   {
+    top: '2rem',
+    right: '14rem',
     width: '43rem',
     minWidth: '43rem',
     height: '50rem',
+    boxShadow: '0 0.3rem 0.6rem rgba(0, 0, 0, 0.2)',
+    backgroundColor: color.white,
   },
 ]);
 
 export const headerWrapper = style([
   utils.flexColumn,
+  utils.borderRadius,
   {
     padding: '2.5rem 2.5rem 0 3rem',
     backgroundColor: color.bg.tab,
