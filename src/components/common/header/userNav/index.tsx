45404365e9d26900688b6c02440ac3552f37ff4e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-06 20:41:19 +0900

    refactor: 불필요한 NotAuthorized 컴포넌트 제거

--- src/components/common/header/userNav/index.tsx
+++ src/components/common/header/userNav/index.tsx
@@ -1,15 +1,25 @@
-import { useRecoilValue } from 'recoil';
-import NotAuthorized from './NotAuthorized';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import Authorized from './Authorized';
+import { Button } from '@/components/common';
 import { isAuthorizedState } from '@/stores/auth';
+import { isLoginModalVisibleState } from '@/stores/modal';
 import * as style from '../style.css';
 
 const UserNav = () => {
   const isAuthorized = useRecoilValue(isAuthorizedState);
+  const setIsLoginModalVisible = useSetRecoilState(isLoginModalVisibleState);
 
   return (
     <div className={style.userNav}>
-      {isAuthorized ? <Authorized /> : <NotAuthorized />}
+      {isAuthorized ? (
+        <Authorized />
+      ) : (
+        <Button
+          isBold={true}
+          text="로그인"
+          onClick={() => setIsLoginModalVisible(true)}
+        />
+      )}
     </div>
   );
 };
