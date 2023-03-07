2c9c61234ae5b333288d197d6a6b52760648d7e4
EUNA <beni1026@naver.com>
2023-03-07 16:04:58 +0900

    fix: import 요소 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,6 +4,4 @@ export { default as HomePage } from '@/pages/home';
 export { default as DailyBriefingPage } from '@/pages/dailyBriefing';
 export { default as MyPage } from '@/pages/myPage';
 export { default as NotFoundPage } from '@/pages/notFound';
-export { default as SearchResultPage } from '@/pages/searchResult';
-export { default as SignupPage } from '@/pages/signup';
 export { default as CreatorListPage } from '@/pages/creatorList';
