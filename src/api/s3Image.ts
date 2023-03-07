3f022afea0f4a9c985fa3b3138827dbfa48284f3
EUNA <beni1026@naver.com>
2023-03-07 18:47:26 +0900

    rename: 함수 이름 수정

--- src/api/s3Image.ts
+++ src/api/s3Image.ts
@@ -1,22 +1,21 @@
 import AWS from 'aws-sdk';
-import axios from 'axios';
 const { VITE_AWS_ACCESS_KEY_ID, VITE_AWS_SECRET_ACCESS_KEY } = import.meta.env;
 
 const region = 'ap-northeast-2';
 const bucket = 'team-jjinsa-hyperlink-bucket';
 
 AWS.config.update({
-  region: region,
+  region,
   accessKeyId: VITE_AWS_ACCESS_KEY_ID,
   secretAccessKey: VITE_AWS_SECRET_ACCESS_KEY,
 });
 
-export const handleFileInput = async (file) => {
+export const uploadFileToS3 = async (file: File) => {
   const upload = new AWS.S3.ManagedUpload({
     params: {
-      Bucket: bucket, // 버킷 이름
+      Bucket: bucket,
       Key: file.name,
-      Body: file, // 파일 객체
+      Body: file,
     },
   });
 
