40296737c76ba2088de9e0319febe35ca16e70ea
EUNA <beni1026@naver.com>
2023-03-07 15:20:50 +0900

    feat: 프로필 수정 api 추가

--- src/api/member.ts
+++ src/api/member.ts
@@ -8,6 +8,12 @@ export type myInfoResponse = {
   profileImage: string;
 };
 
+export type myNewInfo = {
+  nickname: string;
+  career: string;
+  careerYear: string;
+};
+
 export type attentionCategory = string[];
 
 export const getMyInfo = async () => {
@@ -27,6 +33,16 @@ export const putAttentionCategory = async (
     const response = await axiosInstance.put('/members/attention-category', {
       attentionCategory: newAttentionCategory,
     });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateMyInfo = async (myNewInfo: myNewInfo) => {
+  try {
+    const response = await axiosInstance.put('/members/update', myNewInfo);
 
     return response;
   } catch (error) {
