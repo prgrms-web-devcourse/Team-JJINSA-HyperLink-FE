53f712d183a7a4fb0942bd5db9bfe2e13706a14d
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-03-08 23:59:25 +0900

    feat: 컨텐츠 수 차트 msw 연동

--- src/components/dailyBriefing/ContentsCountChart.tsx
+++ src/components/dailyBriefing/ContentsCountChart.tsx
@@ -1,5 +1,3 @@
-import { Card, Heading } from '../common';
-import * as style from './style.css';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,6 +10,10 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { Card, Heading } from '@/components/common';
+import { CONTENTS_COUNT } from '@/utils/constants/storage';
+import { getItem, setItem } from '@/utils/storage';
+import * as style from './style.css';
 
 ChartJS.register(
   CategoryScale,
@@ -43,28 +45,63 @@ const labels = [
   '23/03/12',
 ];
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      fill: true,
-      label: '누적 콘텐츠 수',
-      data: [10, 12, 34, 54, 56, 75, 84],
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      tension: 0.5,
-    },
-  ],
+type contentsCountChartProps = {
+  standardDate: string;
+  count: number;
 };
 
-const ContentsCountChart = () => {
+const ContentsCountChart = ({
+  standardDate,
+  count,
+}: contentsCountChartProps) => {
+  const countData: contentsCountChartProps[] = getItem(CONTENTS_COUNT, []);
+
+  const lastData = countData[countData.length - 1];
+
+  if (!lastData || lastData.standardDate !== standardDate) {
+    setItem(CONTENTS_COUNT, [
+      ...countData,
+      {
+        standardDate,
+        count,
+      },
+    ]);
+  } else if (lastData.count !== count) {
+    countData.pop();
+    setItem(CONTENTS_COUNT, [
+      ...countData,
+      {
+        standardDate,
+        count,
+      },
+    ]);
+  }
+
+  const chartData = {
+    labels: countData
+      ? countData.slice(-7).map(({ standardDate }) => standardDate)
+      : labels,
+    datasets: [
+      {
+        fill: true,
+        label: '누적 콘텐츠 수',
+        data: countData
+          ? countData.slice(-7).map(({ count }) => count)
+          : [10, 12, 34, 54, 56, 75, 84],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        tension: 0.5,
+      },
+    ],
+  };
+
   return (
     <Card type="default" color="#F7F9FB">
       <div className={style.title}>
         <Heading level={6}>콘텐츠 수</Heading>
       </div>
       <div className={style.chart}>
-        <Line options={options} data={data} />
+        <Line options={options} data={chartData} />
       </div>
     </Card>
   );
