ee3b2536bc6485425735c55fafa0db1f967c65f3
EUNA <beni1026@naver.com>
2023-03-09 16:30:59 +0900

    feat: 에러 뷰 수정

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -2,18 +2,18 @@ import { Heading, Spinner } from '@/components/common';
 import * as style from './style.css';
 import { getMyInfo } from '@/api/member';
 import { useQuery } from '@tanstack/react-query';
-import { MyInfo } from '@/components/myPage';
+import { ErrorView, MyInfo } from '@/components/myPage';
 import { myInfo } from '@/types/myInfo';
 import { useRecoilValue } from 'recoil';
 import { isAuthorizedState } from '@/stores/auth';
 
 const MyPage = () => {
   const isAuthorized = useRecoilValue(isAuthorizedState);
-  if (!isAuthorized) return <h3>다시 로그인 해주세요</h3>;
+  if (!isAuthorized) return <ErrorView message="다시 로그인 해주세요" />;
 
   const { data, isLoading, isError } = useQuery(['myInfo'], getMyInfo);
 
-  if (isError) return <h3>에러 발생</h3>;
+  if (isError) return <ErrorView message="잠시 후 다시 시도해주세요" />;
   if (isLoading) return <Spinner size="huge" />;
 
   const myInfo = data as myInfo;
