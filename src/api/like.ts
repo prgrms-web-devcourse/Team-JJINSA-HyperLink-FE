b2f92b0995ce7c48061372cbffd25030da8424bd
guno517 <guno5170@gmail.com>
2023-03-09 22:03:35 +0900

    feat: 좋아요 기능 api 연동

--- src/api/like.ts
+++ src/api/like.ts
@@ -1,11 +1,12 @@
 import { axiosInstance } from '@/api/core';
 
 export const postLikeResponse = async (contentId: number, type: boolean) => {
-  const isLiked = type ? 0 : 1;
-
   try {
     const response = await axiosInstance.post(
-      `/like/${contentId}&type=${isLiked}`
+      `/like/${contentId}`,
+      {
+        addLike: `${!type}`,
+      }
       // `/like?id=${contentId}&type=${isLiked}` // msw 전용
     );
 
