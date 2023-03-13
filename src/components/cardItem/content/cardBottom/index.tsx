b89eee95decaedbf1006a449aae740c515fbf021
EUNA <beni1026@naver.com>
2023-03-13 20:12:35 +0900

    fix: 크리에이터 추천 제외 시 ui 변경 안되도록 수정

--- src/components/cardItem/content/cardBottom/index.tsx
+++ src/components/cardItem/content/cardBottom/index.tsx
@@ -11,7 +11,6 @@ import { postNotRecommendResponse } from '@/api/notRecommend';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { isLoginModalVisibleState } from '@/stores/modal';
 import { isAuthorizedState } from '@/stores/auth';
-import NotRecommend from '../notRecommend';
 import { selectedTabState } from '@/stores/tab';
 
 type CardBottomProps = {
@@ -30,8 +29,6 @@ const CardBottom = ({
   recommendations,
 }: CardBottomProps) => {
   const navigate = useNavigate();
-  const [isNotRecommendComponentVisible, setIsNotRecommendComponentVisible] =
-    useState(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const isAuthorized = useRecoilValue(isAuthorizedState);
   const setIsLoginModalVisible = useSetRecoilState(isLoginModalVisibleState);
@@ -66,7 +63,6 @@ const CardBottom = ({
       return;
     }
     notRecommendResponse.refetch();
-    setIsNotRecommendComponentVisible(true);
   };
 
   const dateFormat = (date: string) => {
@@ -76,65 +72,58 @@ const CardBottom = ({
   return (
     <div className={style.cardBottom}>
       {isModalVisible && <div className={style.cardBackgroundDim} />}
-      {isNotRecommendComponentVisible ? (
-        <NotRecommend />
-      ) : (
-        <>
-          <div className={style.bottomContent}>
-            <div className={style.bottomInfo}>
-              <div style={{ display: 'flex' }}>
-                <span
-                  className={style.bottomInfoCreator}
-                  onClick={handleCreatorClick}
-                >
-                  {creatorName}
-                </span>
-                <Divider type="vertical" />
-                <span>{dateFormat(createdAt)}</span>
-              </div>
-              <CardModal
-                isOpen={isModalVisible}
-                onClose={() => setIsModalVisible(false)}
-                style={{ top: '2.2rem', right: '1rem', zIndex: 2000 }}
+      <>
+        <div className={style.bottomContent}>
+          <div className={style.bottomInfo}>
+            <div style={{ display: 'flex' }}>
+              <span
+                className={style.bottomInfoCreator}
+                onClick={handleCreatorClick}
               >
-                <div
-                  className={style.notRecommended}
-                  onClick={handleNotRecommendClick}
-                >
-                  <Icon
-                    type="regular"
-                    name="face-tired"
-                    size="large"
-                    color="lightgray"
-                    style={{ marginRight: '0.5rem' }}
-                  />
-                  해당 크리에이터 추천 안함
-                </div>
-              </CardModal>
+                {creatorName}
+              </span>
+              <Divider type="vertical" />
+              <span>{dateFormat(createdAt)}</span>
+            </div>
+            <CardModal
+              isOpen={isModalVisible}
+              onClose={() => setIsModalVisible(false)}
+              style={{ top: '2.2rem', right: '1rem', zIndex: 2000 }}
+            >
               <div
-                onClick={handleDotIconClick}
-                className={style.bottomEllipsis}
+                className={style.notRecommended}
+                onClick={handleNotRecommendClick}
               >
-                <Icon type="solid" name="ellipsis-vertical" />
+                <Icon
+                  type="regular"
+                  name="face-tired"
+                  size="large"
+                  color="lightgray"
+                  style={{ marginRight: '0.5rem' }}
+                />
+                해당 크리에이터 추천 안함
               </div>
+            </CardModal>
+            <div onClick={handleDotIconClick} className={style.bottomEllipsis}>
+              <Icon type="solid" name="ellipsis-vertical" />
             </div>
-            <div className={style.bottomTitle}>{title}</div>
           </div>
-          <footer className={style.companyBanner}>
-            <BannerAvatar companies={recommendations} />
-            <div style={{ flexGrow: 1 }}>
-              <BannerText companies={recommendations} />
-              {recommendations?.length === 0 ? (
-                <div className={style.companyText}>관심을 가지지 않았어요</div>
-              ) : (
-                <div className={style.companyText}>
-                  사람들도 관심있게 보고있어요
-                </div>
-              )}
-            </div>
-          </footer>
-        </>
-      )}
+          <div className={style.bottomTitle}>{title}</div>
+        </div>
+        <footer className={style.companyBanner}>
+          <BannerAvatar companies={recommendations} />
+          <div style={{ flexGrow: 1 }}>
+            <BannerText companies={recommendations} />
+            {recommendations?.length === 0 ? (
+              <div className={style.companyText}>관심을 가지지 않았어요</div>
+            ) : (
+              <div className={style.companyText}>
+                사람들도 관심있게 보고있어요
+              </div>
+            )}
+          </div>
+        </footer>
+      </>
     </div>
   );
 };
