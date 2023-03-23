c4fc9de6e89447975fc3043cdca70a464bcae236
류건열 <rjsduf0503@naver.com>
2023-03-23 21:03:26 +0900

    feat: back to top fab에 툴팁 적용

--- src/components/backToTop/index.tsx
+++ src/components/backToTop/index.tsx
@@ -1,4 +1,4 @@
-import { FAB, Icon } from '../common';
+import { FAB, Icon, Tooltip } from '@/components/common';
 import * as variants from '@/styles/variants.css';
 import { scrollTo } from '@/utils/scroll';
 
@@ -9,9 +9,11 @@ type BackToTopProps = {
 
 const BackToTop = ({ onClick, visible }: BackToTopProps) => {
   return (
-    <FAB onClick={onClick || (() => scrollTo(window, 0))} visible={visible}>
-      <Icon name="angles-up" color={variants.color.primary} />
-    </FAB>
+    <Tooltip message="상단으로 이동" position="top-end">
+      <FAB onClick={onClick || (() => scrollTo(window, 0))} visible={visible}>
+        <Icon name="angles-up" color={variants.color.primary} />
+      </FAB>
+    </Tooltip>
   );
 };
 
