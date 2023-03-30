16b0bd2cb225864362d4d6922f8b0acc0ea1575c
EUNA <beni1026@naver.com>
2023-03-30 17:53:19 +0900

    feat: 프로필 페이지 반응형 구현

--- src/pages/myPage/style.css.ts
+++ src/pages/myPage/style.css.ts
@@ -5,9 +5,19 @@ export const wrapper = style([
   utils.flexColumn,
   {
     margin: '0 auto',
-    padding: '10rem',
-    width: '60rem',
-    minWidth: '60rem',
-    gap: '3rem',
+    '@media': {
+      'screen and (max-width: 943px)': {
+        padding: '3rem 0',
+        width: '30rem',
+        minWidth: '30rem',
+        gap: '2rem',
+      },
+      'screen and (min-width: 943px)': {
+        padding: '10rem',
+        width: '60rem',
+        minWidth: '60rem',
+        gap: '3rem',
+      },
+    },
   },
 ]);
