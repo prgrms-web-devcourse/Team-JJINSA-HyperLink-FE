1f12c23fed9f1705b3fd4d894b43da42882939e2
EUNA <beni1026@naver.com>
2023-05-02 15:31:22 +0900

    refactor: 공통 media 사용

--- src/pages/myPage/style.css.ts
+++ src/pages/myPage/style.css.ts
@@ -1,23 +1,20 @@
 import { style } from '@vanilla-extract/css';
 import * as utils from '@/styles/utils.css';
+import { medium } from '@/styles/medias.css';
 
 export const wrapper = style([
   utils.flexColumn,
+  medium({
+    padding: '3rem 0',
+    width: '30rem',
+    minWidth: '30rem',
+    gap: '2rem',
+  }),
   {
     margin: '0 auto',
-    '@media': {
-      'screen and (max-width: 943px)': {
-        padding: '3rem 0',
-        width: '30rem',
-        minWidth: '30rem',
-        gap: '2rem',
-      },
-      'screen and (min-width: 943px)': {
-        padding: '10rem',
-        width: '60rem',
-        minWidth: '60rem',
-        gap: '3rem',
-      },
-    },
+    padding: '10rem',
+    width: '60rem',
+    minWidth: '60rem',
+    gap: '3rem',
   },
 ]);
